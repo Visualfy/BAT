67f7998956b0441ca0cbf3c10f1f0b7f706b4696
Juan Carlos <juancarlos@secmotic.com>
2018-10-05 11:34:31 +0200

    Ahora al crear proyectos solo se pueden seleccionar clases raiz.

--- annotation_tool/serializers.py
+++ annotation_tool/serializers.py
@@ -38,7 +38,7 @@ class ProjectSerializer(serializers.Serializer):
     # All the names of Class objects are loaded to the classes field
     # http://programtalk.com/python-examples/aiorest_ws.utils.fields.flatten_choices_dict/
     def __init__(self, *args, **kwargs):
-        classes = models.Class.objects.all()
+        classes = models.Class.objects.filter(root = 1).exclude(name ='Root')
         class_dict = dict([(c.name, c.id) for c in classes])
         self.fields['classes'].grouped_choices = to_choices_dict(class_dict)
         self.fields['classes'].choices = flatten_choices_dict(self.fields['classes'].grouped_choices)   
