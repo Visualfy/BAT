3696c985f6f0a87eab741aae8b862745b575f82d
juanan <juanan-secmotic@acurable.com>
2018-10-29 13:07:15 +0100

    tree structure with event managed finished

--- annotation_tool/js/project.js
+++ annotation_tool/js/project.js
@@ -1,42 +1,46 @@
 $(function () {
-  // delete items with ajax DELETE request
-  $('.delete-item').on('click', function () {
-    var url = $(this).data('delete-url');
-    $('#delete-url').val(url);
-  });
-  $('#delete-button').on('click', function () {
-    $.ajax({
-      url: $('#delete-url').val(),
-      beforeSend: function(xhr) {
-          xhr.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
-      },
-      type: 'DELETE',
-      success: function () {
-        //reset page
-        location.reload()
-      }
-    });
-  });
+    // delete items with ajax DELETE request
+    recreateDelete();
+    function recreateDelete(){
+        $('.delete-item').off('click');
+        $('.delete-item').on('click', function () {
+            var url = $(this).data('delete-url');
+            $('#delete-url').val(url);
+        });
+        $('#delete-button').on('click', function () {
+            $.ajax({
+                url: $('#delete-url').val(),
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
+                },
+                type: 'DELETE',
+                success: function () {
+                    //reset page
+                    location.reload()
+                }
+            });
+        });
+    }
 
-  // autocomplete for tag field
-  if (typeof TAGS_NAMES != 'undefined') {
-    $('#tokenfield').tokenfield({
-      autocomplete: {
-        source: TAGS_NAMES,
-        delay: 100
-      },
-      showAutocompleteOnFocus: true
-    })
-  }
+    // autocomplete for tag field
+    if (typeof TAGS_NAMES != 'undefined') {
+        $('#tokenfield').tokenfield({
+            autocomplete: {
+                source: TAGS_NAMES,
+                delay: 100
+            },
+            showAutocompleteOnFocus: true
+        })
+    }
 
-  // open modal when page was opened
-  if (typeof OPEN_MODAL != 'undefined' && OPEN_MODAL) {
-    $('#add-item-button').trigger('click');
-  }
+    // open modal when page was opened
+    if (typeof OPEN_MODAL != 'undefined' && OPEN_MODAL) {
+        $('#add-item-button').trigger('click');
+    }
 
-  // color picker init
-  var color_input = $('#color-picker');
-  if (color_input.length) {
-    color_input.colorpicker({'format': 'rgb'});
-  }
+    // color picker init
+    var color_input = $('#color-picker');
+    if (color_input.length) {
+        color_input.colorpicker({'format': 'rgb'});
+    }
 });
